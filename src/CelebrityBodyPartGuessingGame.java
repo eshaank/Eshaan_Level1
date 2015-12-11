3132ba2aa35ba9caa23400d45134a9d9b4cea835
BigRockerEnding <bigrockerending@live.com>
2015-12-10 19:55:10 -0800

    fas
    ads

--- src/CelebrityBodyPartGuessingGame.java
+++ src/CelebrityBodyPartGuessingGame.java
@@ -11,26 +11,50 @@ public class CelebrityBodyPartGuessingGame {
 
 	// Get 3 celebrity photos from the internet, save them to your computer, and fill
 	// in their paths here. To get the path, right click on the image, and copy its Location.
-	String firstImage = "images.jpg";
-	String secondImage = "Adam+Savage+Pacific+Rim+Premieres+LA+MQNwR-gKkbLl.jpg";
-	String thirdImage = "imgres.jpg";
+	String firstImage = "/Users/league/Desktop/Eshaan_Level1/src/images.jpg";
+	String secondImage = "/Users/league/Desktop/Eshaan_Level1/src/Adam+Savage+Pacific+Rim+Premieres+LA+MQNwR-gKkbLl.jpg";
+	String thirdImage = "/Users/league/Desktop/Eshaan_Level1/src/CARRIE FISHER.jpg";
 
 	Frame window = new Frame();
 	
 	private void startQuiz() {
 		// Set the size of the window so that you can only see part of the image.
-			window.setSize(500, 500);
+			window.setSize(50, 50);
 		// Make an int variable to hold the score.
-
+int score = 0;
 		// Ask the user who this person is and store their answer
+String han = JOptionPane.showInputDialog("Who is this pseron (don't forget caps)");
+		
+if (han.equals("Harrison Ford")) {
+	score+=1;
+	JOptionPane.showMessageDialog(null, "You're correct.Your score is now " + score);
+}
+else {
+	JOptionPane.showMessageDialog(null, "Your're wrong");
+}
+showNextImage();
+window.setSize(500, 75);
+String adam = JOptionPane.showInputDialog("Who is this pseron");
 
-		// If they got the answer right:
-		// -- Tell them they are correct.
-		// -- Increase the score by 1
+if (adam.equals("Adam Savage")) {
+	score+=1;
+	JOptionPane.showMessageDialog(null, "You're correct. Your score is now " + score);
+}
+else {
+	JOptionPane.showMessageDialog(null, "Your're wrong");
+}
 
-		// Otherwise:
-		// -- Tell them they are wrong
+showNextImage();
+window.setSize(400, 400);
+String leia = JOptionPane.showInputDialog("Who is this person");
 
+if (leia.equals("Carrie Fisher")) {
+	score+=1;
+	JOptionPane.showMessageDialog(null, "You're correct. Your score is now " + score);
+}
+else {
+	JOptionPane.showMessageDialog(null, "Your're wrong " + score);
+}
 		// Use the method below to show the next image
 		// Show the score
 		// .... repeat for all your images.....
