496eed2a1d3c43e5dd5ab9a4e3cea34116ea3925
PhazeIllusions <league@WTS14.attlocal.net>
2016-01-07 19:58:36 -0800

    Skyline
    AWESOME. RAD M8

---
+++ src/skyline.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
+
+public class skyline {
+	public static void main(String[] args) {
+		Tortoise.show();
+		Tortoise.setX(100);
+		Tortoise.setY(300);
+		for (int i = 0; i < 9; i++) {
+			int hi = new Random().nextInt(200);
+			drawBuilding("small");
+			drawBuilding("medium");
+			drawBuilding("large");
+		}
+
+	}
+
+	static void drawBuilding(String size) {
+
+		Tortoise.setSpeed(10);
+		Tortoise.setPenColor(Colors.getRandomColor());
+		if (size.equals("small")) {
+			Tortoise.move(50);
+		}
+		if (size.equals("medium")) {
+			Tortoise.move(100);
+		}
+		if (size.equals("large")) {
+			Tortoise.move(200);
+		}
+		Tortoise.turn(90);
+		Tortoise.move(50);
+		Tortoise.turn(90);
+		if (size.equals("small")) {
+			Tortoise.move(50);
+		}
+		if (size.equals("medium")) {
+			Tortoise.move(100);
+		}
+		if (size.equals("large")) {
+			Tortoise.move(200);
+		}
+		Tortoise.turn(-90);
+		Tortoise.move(50);
+		Tortoise.turn(270);
+		// Tortoise.move(50);
+	}
+}
