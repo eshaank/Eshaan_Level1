bcecb66ba1af102f183c8d46d75a413d5276f5ee
Mrfuzzy <league@iMac.attlocal.net>
2016-01-16 10:42:12 -0800

    finished for now!!
    HARD _________

--- src/myOwnProject.java
+++ src/myOwnProject.java
@@ -34,27 +34,30 @@ public class myOwnProject extends JPanel implements Runnable, MouseMotionListene
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		System.out.println(mouseColor);
 		int line = -16738816;
-		if (mouseColor == -16738816) {
-			JOptionPane.showMessageDialog(null, "You Died");
-			System.exit(0);
-		}
+//		if (mouseColor == -16738816) {
+//			JOptionPane.showMessageDialog(null, "You Died");
+//			System.exit(0);
+//		}
 		if (mouseColor == -8257410) {
-			JOptionPane.showMessageDialog(null, "You Win!!");
 			scare();
+			JOptionPane.showMessageDialog(null, "You Win!!");
 			System.exit(0);
 		}
 	}
 
 	private void scare() {
-		// 7. find a scary sound and put it in the "default package" where you
-		// put your maze picture. You can find a sound on freesound.org. Log in
-		// as leagueofamazing/code4life.
 		 AudioClip sound =
 		 JApplet.newAudioClip(getClass().getResource("76415__robinhood76__01142-applause-42-crowd-long-waiting.wav"));
-		// 8. play the scary sound. Hint: type "sound" and then a period.
 sound.play();
-		// 9. drop an image into your default package, and use the
-		// showScaryImage method to scare your victim!
+		
+
+	}
+
+	private void sCare() {
+		 AudioClip sound =
+		 JApplet.newAudioClip(getClass().getResource("146434__copyc4t__dundundunnn.wav"));
+sound.play();
+		
 
 	}
 
