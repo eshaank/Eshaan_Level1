4887b678bc876c9a4469c8353d89368805e810ad
Livie0423 <league@WTS9.attlocal.net>
2016-08-10 20:06:35 -0700

    flappy bird
    ads

--- src/Team.java
+++ src/Team.java
@@ -2,6 +2,7 @@
 public class Team {
 	public static void main(String[] args) {
 		Olympian Ervin = new Olympian("Anthony Ervin", 0, 35, "swimming", "USA");
-		Olympian Phelps = new Olympian("Michael Pehlps", 14, 31, "Swimming", "USA");
+		Olympian Phelps = new Olympian("Michael Pehlps", 21, 31, "Swimming", "USA");
+
 	}
 }
