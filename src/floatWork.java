71f41c822d97a8bfb40a9a590db2f8dc35a4f301
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2016-08-28 15:13:57 -0700

    Floats
    float

---
+++ src/floatWork.java
@@ -0,0 +1,14 @@
+
+public class floatWork {
+	
+public static void main(String[] args) {
+	 double int1 = 10.456634563425235346;
+	System.out.println(floatChanger(int1) + ".");
+	
+}
+static int floatChanger(double num1){
+	int j = (int)num1;
+	return j;
+	
+}
+}
